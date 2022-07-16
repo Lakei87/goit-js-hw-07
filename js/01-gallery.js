c5255e81841daf85fdb4aab18ac375c99223d6c5
Lakei87 <monastyrskyis@gmail.com>
2022-07-16 14:59:13 +0200

    completes home tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery")
+galleryRef.insertAdjacentHTML("afterbegin", createMarkup(galleryItems))
+galleryRef.addEventListener("click", showTheOriginalPicture)
+
+
+function createMarkup(array) {
+    return array.map(element => {
+        const { original, preview, description } = element
+        
+        return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img 
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"/>
+            </a>
+        </div>`
+    }).join("")
+}
+
+function showTheOriginalPicture(event) {
+    event.preventDefault()
+    const { target } = event
+    if (!target.dataset.source) return
+    
+    const instance = basicLightbox.create(`<img src="${target.dataset.source}">`, {
+        onShow: () => addEventListener("keydown", checkeTheEscapeButtonToExit),
+        onClose: () => removeEventListener("keydown", checkeTheEscapeButtonToExit)
+    })
+    
+    instance.show()
+    
+    function checkeTheEscapeButtonToExit(event) {
+        if (event.code === "Escape") instance.close()
+    }
+    
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery")
+galleryRef.insertAdjacentHTML("afterbegin", createMarkup(galleryItems))
+galleryRef.addEventListener("click", showTheOriginalPicture)
+
+function createMarkup(array) {
+    return array.map(element => {
+        const { original, preview, description } = element
+        
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" /></a>`
+    }).join("")
+}
+
+function showTheOriginalPicture(event) {
+    event.preventDefault()
+    const { target } = event
+    if (!target.src) return
+
+    let gallery = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+    })
+
+    gallery.on("closed.simplelightbox", function () {
+        gallery.refresh()
+    })
+
+}
