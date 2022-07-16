2df6371c2b2ac58b5d8b9d077d63c625e64692f9
Lakei87 <monastyrskyis@gmail.com>
2022-07-16 15:29:08 +0200

    debugs before mentors reviews

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,7 +5,6 @@ console.log(galleryItems);
 
 const galleryRef = document.querySelector(".gallery")
 galleryRef.insertAdjacentHTML("afterbegin", createMarkup(galleryItems))
-galleryRef.addEventListener("click", showTheOriginalPicture)
 
 function createMarkup(array) {
     return array.map(element => {
@@ -16,18 +15,7 @@ function createMarkup(array) {
     }).join("")
 }
 
-function showTheOriginalPicture(event) {
-    event.preventDefault()
-    const { target } = event
-    if (!target.src) return
-
-    let gallery = new SimpleLightbox(".gallery a", {
-        captionsData: "alt",
-        captionDelay: 250,
-    })
-
-    gallery.on("closed.simplelightbox", function () {
-        gallery.refresh()
-    })
-
-}
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+})
